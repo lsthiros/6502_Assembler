4150b0b943c7ea86b424918e97a7679d9141d9f2
Louie Thiros <lsthiros@gmail.com>
2019-01-19 00:17:43 -0800

    Added hash table for labels. Never again.

--- ParseTree.c
+++ ParseTree.c
@@ -1,9 +1,79 @@
 #include "ParseTree.h"
 
+#include <stdint.h>
+
 typedef struct {
-    unsigned long key;
-    char *value;
-} *Record;
+    char *key;
+    int value;
+    uint8_t valid;
+} Record;
+
+typedef struct {
+    unsigned int size;
+    unsigned int filled;
+    Record *records;
+} Table;
+
+static unsigned long sdbm(uint8_t *str);
+
+static void innerEntry(Record *records, unsigned int size, char *key, int value) {
+    unsigned long seed = sdbm(key) % size;
+    Record *current = &(records[seed]);
+
+    while (current->valid) {
+        seed = (seed + 1) % size;
+        current = &(current[seed]);
+    }
+
+    current->key = key;
+    current->value = value;
+    current->valid = 1;
+}
+
+Table *initTable() {
+    Table *ret = malloc(sizeof(Table));
+    ret->records = calloc(sizeof(Record), 128);
+    ret->filled = 0;
+    ret->size = 128;
+
+    return ret;
+}
+
+uint8_t searchTable(Table *table, char *key)
+
+static void rehash(Table *oldTable) {
+    int idx;
+    int newsize = oldTable->size * 2;
+    Record *current;
+    Record *newHash = calloc(sizeof(Record), newsize);
+
+    for (idx = 0; idx < oldTable->size; idx++) {
+        current = &(oldTable->records[idx]);
+        if (current->valid) {
+            innerEntry(newHash, newsize, current->key, current->value);
+        }
+    }
+
+    free(oldTable->records);
+    oldTable->records = newHash;
+    oldTable->size = newsize;
+}
+
+void insertTable(Table *table, char *key, int value) {
+    char *newKey = strdup(key);
+
+    if (table->filled * 2 > table->size) {
+        rehash(table);
+    }
+
+    innerEntry(table->records, table->size, newKey, value);
+    table->filled++;
+}
+
+void destroyTable(Table *table) {
+    free(table->records);
+    free(table);
+}
 
 /* sdbm hash function */
 static unsigned long sdbm(uint8_t *str) {
