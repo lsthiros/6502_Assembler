8018d740f252e7fa899cdb01077248ae2f59f1da
Louie Thiros <lsthiros@gmail.com>
2019-01-25 11:15:02 -0800

    Cataloging actual bytecodes

--- Emitter.c
+++ Emitter.c
@@ -1,1 +1,30 @@
 #include "Emitter.h"
+
+typedef struct ByteCodeNode {
+    ByteCode *code;
+    struct ByteCodeNode *next;
+} ByteCodeNode;
+
+typedef struct Program {
+    ByteCodeNode *head;
+    ByteCodeNode *tail;
+} Program;
+
+int emitProgram(OpList *list, Program **ret) {
+    Operation *op;
+
+    beginIteration(list);
+}
+
+static const OpCodeType relTypes[] = {
+    BCC, BCS, BEQ, BMI, BNE, BPL, BVC, BVS
+};
+
+static int isRelative(OpCodeType type) {
+    int idx = 0;
+
+    while(idx < 8 && type != relTypes[idx])
+        idx++;
+
+    return type == relTypes[idx];
+}
--- Emitter.h
+++ Emitter.h
@@ -2,6 +2,7 @@
 #define EMITTER_H
 
 #include "OpCodeDefs.h"
+#include "ParseTree.h"
 #include <stdint.h>
 
 typedef struct Program Program;
@@ -10,9 +11,10 @@ typedef struct ByteCode {
     unsigned int location;
     OpCodeType type;
     unsigned int jumpLocation;
+    char *jumpLabel;
     unsigned int length;
     uint8_t code[3];
 } ByteCode;
 
-
+int emitProgram(OpList *list, Program **ret);
 #endif
